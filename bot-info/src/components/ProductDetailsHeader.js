9fc6442f81151363096122f78c302acc369a688e
hafizsheetab <muhtaseen@iut-dhaka.edu>
2022-03-12 23:01:04 +0600

    Update ProductDetailsHeader.js

--- bot-info/src/components/ProductDetailsHeader.js
+++ bot-info/src/components/ProductDetailsHeader.js
@@ -37,34 +37,36 @@ const ProductDetailsHeader = () => {
           </Navbar.Collapse>
         </Container>
         <div className="notification">
-          <i class="fa-solid fa-bell fa-xl" onClick={handleShow}></i>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Notification Log</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Message</th>
-                    <th>Time</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>154</td>
-                    <td>10.53</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="warning" onClick={handleClose}>
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
+            <i class="fa-solid fa-bell fa-xl" onClick={handleShow}>
+              <span className="badge">3</span>
+            </i>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Notification Log</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Message</th>
+                      <th>Time</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>154</td>
+                      <td>10.53</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="warning" onClick={handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
       </Navbar>
     </div>
   );
